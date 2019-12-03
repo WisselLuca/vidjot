c195d2bafbfde662816f43f772ebf5f698742281
Luca <wissel.luca@gmail.com>
2019-12-03 22:45:40 +0100

    Added a Middleware

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 
 //How Middleware works
-
-
+/*app.use(function(req, res, next){
+    console.log(Date.now());
+    req.name ='Luca';
+    next();
+})
+*/
 
 //Index Routing
 app.get('/', (req, res) =>{
+    //console.log(req.name);
     res.send('index');
 });
 
