9af27f00652af8c1c40aac9b26ac1ccb9108dd60
Luca <wissel.luca@gmail.com>
2020-01-03 16:25:11 +0100

    -User Registration form -save user to db -unique email - crypt password

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+//const passport = require('passport');
 
 //Load Idea Model
-//require('../models/Ideas');
-//const Idea = mongoose.model('ideas');
+require('../models/User');
+const User = mongoose.model('users');
 
 //User login Routes
 router.get('/login', (req, res)=>{
@@ -33,7 +35,34 @@ router.post('/register', (req, res)=>{
             passwordConfirm: req.body.passwordConfirm
         });
     }else{
-        res.send('passed')
+        User.findOne({email:req.body.email})
+            .then(user =>{
+                if(user){
+                    req.flash('error_msg', 'Email Already in Use');
+                    res.redirect('/users/register');
+                }else{
+                    const newUser = User({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: req.body.password
+                    });
+                    bcrypt.genSalt(10, (err, salt)=>{
+                        bcrypt.hash(newUser.password, salt, (err, hash)=>{
+                            if(err) throw err;
+                            newUser.password = hash;
+                            newUser.save()
+                                .then(user =>{
+                                    req.flash('success_msg', 'Regstration Compleated');
+                                    res.redirect('/users/login');
+                                })
+                                .catch(err=>{
+                                    console.log(err);
+                                    return;
+                                })
+                        });
+                    });
+                }
+        });
     }
 });
 
